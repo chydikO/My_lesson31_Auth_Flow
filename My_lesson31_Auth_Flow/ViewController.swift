45ec6321bc801f90bd1d5746f4744b556115f9df
Oleg Chudnovskiy MB_Pro_13 <oleg.chudnovskiy@gmail.com>
2020-07-13 19:49:32 +0300

    empty project

---
+++ My_lesson31_Auth_Flow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My_lesson31_Auth_Flow
+//
+//  Created by OlegChudnovskiy on 13.07.2020.
+//  Copyright © 2020 OlegChudnovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
